ac1e82a21e9ad615ec2a8a05bf2ebb32b493c256
Inguim <igorazevedost@gmail.com>
2020-10-20 08:57:00 -0200

    Login, Register e Menu principal feitos

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,76 +2,61 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <form method="POST" action="{{ route('register') }}" class="form-group col-md-12">
+        <h1 class="text-lg-center">{{ __('Cadastrar') }}</h1>
+        @csrf
+        <div class="form-group col-md-12 d-flex justify-content-between" id="form-group-double">
+            <input id="name" type="text" class="input form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="NOME">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+            @error('name')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+            <input id="sobrenome" type="text" class="input form-control @error('sobrenome') is-invalid @enderror" name="sobrenome" value="{{ old('sobrenome') }}" required autocomplete="sobrenome" autofocus placeholder="SOBRENOME">
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+            @error('sobrenome')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+        <div class="form-group row">
+            <div class="col-md-12">
+                <input id="email" type="email" class="input form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="EMAIL">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div>
         </div>
-    </div>
+
+        <div class="form-group row">
+            <div class="col-md-12">
+            <input id="password" type="password" class="input form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="SENHA">
+
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-md-12">
+                <input id="password-confirm" type="password" class="input form-control" name="password_confirmation" required autocomplete="new-password" placeholder="CONFIRMAR SENHA">
+            </div>
+        </div>
+        <div class="form-group row mb-0 justify-content-center">
+            <button type="submit" id="button" class="col-8">
+                {{ __('Cadastrar') }}
+            </button>
+            <a href="{{ route('login') }}">Ja possui uma conta? <span>Logar</span></a>
+        </div>
+    </form>
 </div>
 @endsection
---
+++ resources/views/layouts/sessao.blade.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Elvis') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/bb1af83337.js" crossorigin="anonymous"></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md shadow-lg navbar-dark justify-content-center" id="navbar-central">
+            <!-- <div class="container" style="margin: 0; width: 100%"> -->
+            <a class="navbar-brand" href="{{ url('/home') }}">
+                <img src="logo.png" alt="Elvis" style="width: 150px; height: 40px; ">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                    @endif
+                    @else
+                    <li class="nav-item">
+                        <button data-toggle="collapse" data-target="#navbar-collapse">
+                            <i class="fas fa-bars icon"></i>
+                        </button>
+
+                    </li>
+                    <li class="nav-item">
+                        <i class="fas fa-cog icon"></i>
+                    </li>
+                    <li class="nav-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fas fa-sign-out-alt icon"></i>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                    @endguest
+                </ul>
+            </div>
+            <!-- </div> -->
+        </nav>
+        <div class="collapse show" id="navbar-collapse">
+            <div class="navbar navbar-expand-md d-flex align-items-center justify-content-between flex-wrap" id="navbar-sub">
+                <a class="d-flex align-items-center">
+                    <i class="fas fa-user icon"></i>
+                    <strong>Perfil</strong>
+                </a>
+                <a class="d-flex align-items-center">
+                    <i class="fas music-alt icon" style="width: 30px; margin-right: 0"><img src="nota.svg" style="width: inherit"></img></i>
+                    <strong>Cifras</strong>
+                </a>
+                <a class="d-flex align-items-center">
+                    <i class="fas fa-music icon"></i>
+                    <strong>Playlists</strong>
+                </a>
+            </div>
+        </div>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+
+</html>
+<!-- onclick="event.preventDefault();
+                                        console.log(document.getElementById('navbar-sub').style.display);
+                                        if(document.getElementById('navbar-sub').style.display === 'none') {
+                                            document.getElementById('navbar-sub').style.display = 'inline';
+                                        } else {
+                                            document.getElementById('navbar-sub').style.display = 'none';
+                                        }" -->
