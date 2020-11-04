7c3cd172f175334ede608a7e512cb0f4e43740dd
gmantovanibrito <gmantovanibrito@gmail.com>
2020-11-04 15:15:51 -0300

    Home teste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.sessao')
 
 @section('content')
-<div class="container">
-    
+<div class="d-flex justify-content-center align-items-center flex-fill"
+            style="background-image: url(caixa.png)">
+        <h1 class="text-lg-center">Seja Bem-Vindo ao Elvis! <br> Seu armazendor Virtural de Cifras</h1>
+        </div>
 </div>
 @endsection
