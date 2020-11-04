40bf49669b4426b7c1e2412451e6083d072c5561
gmantovanibrito <gmantovanibrito@gmail.com>
2020-11-04 15:18:50 -0300

    Home teste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.sessao')
 
 @section('content')
-<div class="d-flex justify-content-center align-items-center flex-fill"
-            style="background-image: url(caixa.png)">
+<div class="d-flex justify-content-center align-items-center">
+        <img src="caixa.png" style="width: 150px;">
         <h1 class="text-lg-center">Seja Bem-Vindo ao Elvis! <br> Seu armazendor Virtural de Cifras</h1>
         </div>
 </div>
