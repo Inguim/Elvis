b47fe75913bba28fcb8fed7139aa4a893a794cb2
gmantovanibrito <gmantovanibrito@gmail.com>
2020-11-04 16:49:38 -0300

    Perfil

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -9,12 +9,12 @@
     <div id="listarcifras">
         <table class="table table-borderless  table-hover" id="listar_cifras">
             <tbody>
-                <a href="{{url('/vercifra')}}">
+                <div href="{{url('/vercifra')}}">
                     <tr class="tr" style="border-bottom: 2px solid #626262;">
                         <td  style="font-size: medium;" >Oasis - Wonderwall </td>
                         <td  style="align-items:initial; text-align:left;"></td>
                     </tr>
-                </a>
+                </div>
                 <tr class="tr" style="border-bottom: 2px solid #626262;">
                     <td  style="font-size: medium;">Nirvana - Smells like teen spirit</td>  
                     <td style="align-items:initial; text-align:left;"></td>
