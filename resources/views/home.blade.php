cf04a29e4a43215094f34d61158b5f4fe63c6da5
gmantovanibrito <gmantovanibrito@gmail.com>
2020-11-04 15:24:32 -0300

    Teste Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="d-flex justify-content-center align-items-center">
         <img src="caixa.png" style="width: 150px;">
-        <div class="justify-content-center">
+</div>
+<div class="justify-content-center">
             <h1 class="text-lg-center">Seja Bem-Vindo ao Elvis! <br> Seu armazendor Virtural de Cifras</h1>
-        </div>
 </div>
 @endsection
