51a18357857d50b8913417c8ed5cd3b3528c4e34
gmantovanibrito <gmantovanibrito@gmail.com>
2020-11-04 15:36:22 -0300

    Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,11 +9,11 @@
         color: #000000;
         margin-top: 50px;
     }
-    
+
 </style>
 
 <div class="d-flex justify-content-center align-items-center">
-        <img src="caixa.png" style="width: 150px; margin-top: 150px;">
+        <img src="caixa.png" style="width: 150px; margin-top: 100px;">
 </div>
 <div class="main">
             <h1 class="text-lg-center">Seja Bem-Vindo ao Elvis! <br> Seu armazendor Virtual de Cifras</h1>
