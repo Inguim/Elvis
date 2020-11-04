a218876aa14f2cc6e97cb4d0391e1fd6abc112bb
gmantovanibrito <gmantovanibrito@gmail.com>
2020-11-04 15:29:52 -0300

    Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,23 @@
 @extends('layouts.sessao')
 
 @section('content')
+
+<style>
+    .main h1 {
+        font-size: 200px;
+        text-align: center;
+        color: #000000;
+        margin-top: 50px;
+    }
+    .div img{
+        margin-top: 100px;
+    }
+</style>
+
 <div class="d-flex justify-content-center align-items-center">
         <img src="caixa.png" style="width: 150px;">
 </div>
-<div class="justify-content-center">
+<div class="main">
             <h1 class="text-lg-center">Seja Bem-Vindo ao Elvis! <br> Seu armazendor Virtural de Cifras</h1>
 </div>
 @endsection
