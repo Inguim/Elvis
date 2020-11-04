e1e6092dc08ce7834bbc9cc915bbc25fb792449a
gmantovanibrito <gmantovanibrito@gmail.com>
2020-11-04 16:29:10 -0300

    Perfil

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -10,9 +10,7 @@
         <table class="table table-borderless  table-hover" id="listar_cifras">
             <tbody>
                 <tr class="tr" style="border-bottom: 2px solid #626262;">
-                    <td  style="font-size: medium;">
-                    <a href="{{url('/vercifra')}}">Oasis - Wonderwall</a>
-                    </td>
+                    <td href="{{url('/vercifra')}}" style="font-size: medium;">Oasis - Wonderwall</td>
                     <td style="align-items:initial; text-align:left;"></td>
                 </tr>
                 <tr class="tr" style="border-bottom: 2px solid #626262;">
